becea0420a9a3a8fb114e77088ace5f6a547f304
Kirby Shabaga <kirby.shabaga@gmail.com>
2017-05-25 10:44:07 -0700

    add setState

--- src/App.js
+++ src/App.js
@@ -4,41 +4,65 @@ import profilePic from './kirby.png';
 import './App.css';
 import Navbar from './containers/Navbar'
 import ProfileCard from './components/ProfileCard'
+
 import Content from './components/Content'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
-const questions = [
-  {
-    question: "Where am I located?",
-    answer: "Bellevue, WA"
-  },
-  {
-    question: "What do I work in?",
-    answer: "Virtual Reality"
-  },
-  {
-    question: "What is my favourite restaurant?",
-    answer: "La Belle Patate (Victoria, BC)"
-  }
-]
-
-const plainText = [
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-]
-
-const quotation = [
-  {
-    quote: "Technology is great...when it works.",
-    author: "Kirby Shabaga",
-    reference: "The Internets"
-  }
-]
+// const questions = [
+//   {
+//     question: "Where am I located?",
+//     answer: "Bellevue, WA"
+//   },
+//   {
+//     question: "What do I work in?",
+//     answer: "Virtual Reality"
+//   },
+//   {
+//     question: "What is my favourite restaurant?",
+//     answer: "La Belle Patate (Victoria, BC)"
+//   }
+// ]
+//
+// const plainText = [
+//   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// ]
+//
+// const quotation = [
+//   {
+//     quote: "Technology is great...when it works.",
+//     author: "Kirby Shabaga",
+//     reference: "The Internets"
+//   }
+// ]
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      content: <Home />
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const id = event.target.id;
+    if (id === "home") {
+      this.setState({content: <Home />});
+    } else if (id === "about") {
+      this.setState({content: <About />});
+    } else if (id === "contact") {
+      this.setState({content: <Contact />});
+    }
+  }
+
   render() {
     return (
       <div className="App">
 
-        <Navbar />
+        <Navbar onClick={(event) => this.handleClick(event)}/>
 
       <div className='page'>
         <div className='container-fluid top-padding'>
@@ -50,10 +74,11 @@ class App extends Component {
               oneLiner="Virtual Reality/Futurist" />
           </div>
           <div className='col-md-8'>
-            <Content
+            <Content content={this.state.content} />
+            {/* <Content
               questions={questions}
               plainText={plainText}
-              quotation={quotation} />
+              quotation={quotation} /> */}
           </div>
         </div>
       </div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -7,30 +7,32 @@ import QuotationComponent from './QuotationComponent'
 class Content extends Component {
   render(){
 
-    var questionSection = this.props.questions.map(function (entry) {
-      return <QuestionAnswerComponent
-        key={entry.question}
-        question={entry.question}
-        answer={entry.answer} />
-    })
+    // var questionSection = this.props.questions.map(function (entry) {
+    //   return <QuestionAnswerComponent
+    //     key={entry.question}
+    //     question={entry.question}
+    //     answer={entry.answer} />
+    // })
+    //
+    // var plainTextSection = this.props.plainText.map(function (entry, index) {
+    //   return <PlainTextComponent key={index} paragraph={entry}/>
+    // })
+    //
+    // var quotationSection = this.props.quotation.map(function(entry) {
+    //   return <QuotationComponent
+    //     key={entry.quote}
+    //     quote={entry.quote}
+    //     author={entry.author}
+    //     reference={entry.reference} />
+    // })
 
-    var plainTextSection = this.props.plainText.map(function (entry, index) {
-      return <PlainTextComponent key={index} paragraph={entry}/>
-    })
+    // return(<div>
+    //   {questionSection}
+    //   {plainTextSection}
+    //   {quotationSection}
+    // </div>)
 
-    var quotationSection = this.props.quotation.map(function(entry) {
-      return <QuotationComponent
-        key={entry.quote}
-        quote={entry.quote}
-        author={entry.author}
-        reference={entry.reference} />
-    })
-
-    return(<div>
-      {questionSection}
-      {plainTextSection}
-      {quotationSection}
-    </div>)
+    return(<div>{this.props.content}</div>);
   }
 }
 
